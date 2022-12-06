fb4cb1b553b3dca5f12ebea74710ad7ca48b3788
PETER KIRSCHNER <peter@klib.io>
2022-12-06 10:11:02 +0000

    improve error message

--- docker-github-runner/docker-build.sh
+++ docker-github-runner/docker-build.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
+set -x
+scriptDir="$( cd $( dirname ${BASH_SOURCE[0]} ) >/dev/null 2>&1 && pwd )"
+[[ -z "${TOKEN}" ]] && echo "please provide your Github PAT token via env variable TOKEN" && exit 1
+
+GITHOST=${GITHOST:-api.github.com}
+ORG=klibio
 
 # activate bash checks for unset vars, pipe fails
 set -eauo pipefail
-SCRIPT_DIR="$( cd $( dirname ${BASH_SOURCE[0]} ) >/dev/null 2>&1 && pwd )"
 
-downloadUrl=$( curl -s \
+downloadUrl=$(curl -s \
     -H "Accept: application/vnd.github+json"\
     -H "Authorization: Bearer ${TOKEN}" \
-    https://${HOSTNAME}/orgs/${ORG}/actions/runners/downloads \
-    | jq -r '.[] | select(.os|test("linux")) | select(.architecture|test("x64")) | .download_url' )
+    https://${GITHOST}/orgs/${ORG}/actions/runners/downloads \
+    | jq -r '.[] | select(.os|test("linux")) | select(.architecture|test("x64")) | .download_url')
 
-version=$(echo $downloadUrl | sed 's,.*download/v\(.*\)/.*,\1,g')
+runnerVersion=$(echo $downloadUrl | sed 's,.*download/v\(.*\)/.*,\1,g')
 
+cd $scriptDir
 docker build \
     --progress=plain \
     --no-cache \
-    --build-arg RUNNER_VERSION=${RUNNER_VERSION} \
+    --build-arg RUNNER_VERSION=${runnerVersion} \
     -t github-action-runner-image \
     .
